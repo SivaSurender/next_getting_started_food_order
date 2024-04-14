5a8d6c055be0664a4148598535f776b6034850be
SivaSurender <e.surender14@gmail.com>
2024-04-14 21:15:34 +0530

    error handling

---
+++ app/meals/error.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+function Error() {
+  return (
+    <main className="error">
+      <p>Error Occurred 🙁</p>
+      <p>Please try again later !!</p>
+    </main>
+  );
+}
+
+export default Error;
