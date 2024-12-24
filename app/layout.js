4f1472f4ce1efa72f9d65e4d902055d486274eb2
Bala E <93850183+SivaSurender@users.noreply.github.com>
2024-12-24 20:35:06 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,23 +13,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
       </body>
-     <p
-          style={{
-            marginTop: "auto",
-            textAlign: "center",
-            color: "var(--color-light--1)",
-          }}
-        >
-          &copy;
-          <a
-            className="twitter-link cursor-pointer"
-            target="_blank"
-            href="https://my-portfolio-bala.vercel.app/"
-          >
-            <b>Bala.E</b>
-          </a>
-          . Done in India with love ♥.
-        </p>
+     
     </html>
   );
 }
